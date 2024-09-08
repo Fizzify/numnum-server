f82ca9d127fdab3e7ecb1f51aa8470afec0d0b20
iakura <inotanglao@gmail.com>
2024-09-08 14:26:00 -0600

    env stuff

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { generatePairs } from "../util/num";
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5500",
+      "numnum-alpha.vercel.app",
+    ],
   },
 });
 
@@ -39,6 +45,6 @@ io.on("connection", async (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:3001`);
 });

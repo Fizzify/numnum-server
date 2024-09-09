6d9572a8ad29468c6250d45b11c6b006c355d509
iakura <inotanglao@gmail.com>
2024-09-09 11:47:50 -0600

    fix bugs

--- util/num.ts
+++ util/num.ts
@@ -5,11 +5,15 @@ export function generatePair(): [number, number, number] {
   const randomNumberTwo = Math.round(Math.random() * 100);
   const answer = randomNumberOne + randomNumberTwo;
 
+  console.log(randomNumberOne);
+  console.log(randomNumberTwo);
+  console.log(answer);
+
   return [randomNumberOne, randomNumberTwo, answer];
 }
 
 export function generateMonsterFileName() {
-  const randomMonsterFileNumber = Math.round(Math.random() * 64);
+  const randomMonsterFileNumber = Math.round(Math.random() * 63) + 1;
 
   const creatureNumber = String(randomMonsterFileNumber).padStart(3, "0");
 
@@ -20,10 +24,11 @@ export function generatePairs(num: number) {
   const pairs: PairType[] = [];
 
   for (let i = 0; i < num; i++) {
+    const generatedPair = generatePair();
     const pair: PairType = {
-      numOne: generatePair()[0],
-      numTwo: generatePair()[1],
-      answer: generatePair()[2],
+      numOne: generatedPair[0],
+      numTwo: generatedPair[1],
+      answer: generatedPair[2],
       image: generateMonsterFileName(),
     };
     pairs.push(pair);
